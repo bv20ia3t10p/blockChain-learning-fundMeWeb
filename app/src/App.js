9fc0ff54c7c68301a67764fed47f591ac1811dde
Phạm Trọng Phúc <94206242+bv20ia3t10p@users.noreply.github.com>
2023-01-07 06:12:26 -0800

    fin

--- app/src/App.js
+++ app/src/App.js
@@ -39,16 +39,47 @@ const listenForTransactionMine = (transactionResponse, provider) => {
   })
 }
 
+const getBalance = async (setBalance) => {
+  if (typeof window.ethereum) {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const balance = await provider.getBalance(contractAddress)
+    setBalance(ethers.utils.formatEther(balance))
+    console.log(balance)
+  }
+}
+
+async function withdraw() {
+  console.log(`Withdrawing...`)
+  if (typeof window.ethereum !== 'undefined') {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    await provider.send('eth_requestAccounts', [])
+    const signer = provider.getSigner()
+    const contract = new ethers.Contract(contractAddress, fundMeABI, signer)
+    try {
+      const transactionResponse = await contract.withdraw()
+      await listenForTransactionMine(transactionResponse, provider)
+      await transactionResponse.wait(1)
+    } catch (error) {
+      console.log(error)
+    }
+  } else {
+    // withdrawButton.innerHTML = "Please install MetaMask"
+  }
+}
+
 function App() {
   const [eth, setEth] = useState(0)
   const [latestFunder, setLatestFunder] = useState('')
   const [latestAmount, getLatestAmount] = useState('')
+  const [balance, setBalance] = useState('')
   return (
     <div className="App">
       <button onClick={() => connect()}>Connect wallet</button>
       <input type="text" value={eth} onChange={(e) => setEth(e.target.value)} />
       <button onClick={() => fund(eth)}>Fund</button>
       <div className="">{latestAmount}</div>
+      <div className="">Balance : {balance}</div>
+      <button onClick={() => getBalance(setBalance)}>Get balance</button>
     </div>
   )
 }
